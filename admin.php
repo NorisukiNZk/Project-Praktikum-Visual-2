f3a98191a21883cccc1be6cb49c88e8aa725a7a8
NorisukiNZk <born.waffa@gmail.com>
2023-11-09 18:50:58 +0800

    Ini Data Admin

---
+++ admin.php
@@ -0,0 +1,8 @@
+<?php include "head-admin.php"; ?>
+<h2>Praktikum Pemrograman Web Halaman Administrator</h2> 
+<hr />
+<div class="well">
+<p>Selamat Datang <?php echo $_SESSION['nama']; ?> </p>
+<p> Selamat datang di praktikum Pemrograman Web dengan PHP, Anda telah mengakses sebagai Admin pada web ini, silakan gunakan menu navigasi untuk mengelola website ini. </p>
+</div>
+<?php include "foot.php"; ?>
